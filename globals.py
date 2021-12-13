2ad5e48d9066b754c5a385778bd1027b866e8c75
I-Dayz-I <61361549+I-Dayz-I@users.noreply.github.com>
2021-12-13 12:20:14 -0500

    Añadiendo Elementos
    Añadiendo los componentes que poblarán el mundo, y una clase Coordenadas que serán usada por las casillas y especies.

--- globals.py
+++ globals.py
@@ -1,3 +1,15 @@
+#Todas las zonas con las que trabaja la simulación
+ZoneList = {'Forest', 'Mountain', 'Ocean'}
 
+#Todos los elementos con los que trabaja la simulación
+
+WorldComponents = { 'Solar Light','Water', 'Organic Matter', 'Phosil' ,'Iron' , 'Aluminum' , 'Oxygen' , 'Carbon Dioxide' , 'Helium' , 'Nitrogen' }
+
+
+LargeRandomRange = (3000, 5000)
+MediumRandomRange = (1000, 3000)
+LowRandomRange = (200, 1000)
+NoneRandomRange = (0, 200)
 
 allSpecies ={}
+
