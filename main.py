bdead0f5383919a231b0c917f6e1351997ceeaf9
I-Dayz-I <61361549+I-Dayz-I@users.noreply.github.com>
2021-12-16 23:57:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,39 @@ def worldController():
             
 
 
+def movementWithBoundries (Individuo, valor_de_percepcion, mapa):
+     perceptionValue = int(valor_de_percepcion)
+     
+     #ESSTO HAY QUE CAMBIARLOOOOO CUANDO SE CAMBIE EL TIPO DE COORDENADAS DE LA ESPECIE
+     actRow = Individuo.xMundo  - perceptionValue
+     actCol = Individuo.yMundo - perceptionValue
+     perceptionList = []
+     ##LLenando perceptionlist
+     for i in range(0, Individuo.xMundo  + 1 + perceptionValue):
+        newList = []
+        for j in range(0, Individuo.yMundo  + 1 + perceptionValue):
+           newList.append("")
+        perceptionList.append(newList)
+        
+     
+     countX=0
+     countY=0
+     
+     for actRow in range(0, Individuo.xMundo  + 1 + perceptionValue):
+        for actCol in range(0, Individuo.yMundo  + 1 + perceptionValue):
+            if not(0<=actRow<= mapa.SizeX) and not(0<=actCol<=mapa.SizeY):
+                perceptionList[countX][countY]= ""
+            else:
+                perceptionList[countX][countY]= mapa.Tiles[actRow][actCol]
+            
+            countY+=1
+        
+        
+     countX+=1
+     
+     return perceptionList
+     
+
     
 def main():
     globals.allSpecies["Alfie"]=especies.Alfie()
