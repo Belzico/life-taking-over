099b9ec80c2623780b9f9a274ceb73f5943d25ff
I-Dayz-I <61361549+I-Dayz-I@users.noreply.github.com>
2021-12-17 02:06:56 -0500

    Revert "Revert "Merge branch 'testMoveSpecies' of https://github.com/Belzico/life-taking-over into testMoveSpecies""
    This reverts commit 976c217d3c575da8772f7fc127576aeeaef8bf57.

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ import map
 def worldController():
     while (True):
         
+        i=10
         value=''
         for val in globals.worldIndividuals:
             value=globals.worldIndividuals[val]
             tempMap=globals.worldMap.movementWithBoundries(value,1) 
             value.resolveIteration(tempMap)
+        i-=1
         
         globals.worldMap.PrintMap()
+        print('---------------------------------NEW CYCLE-----------------------------------------------------------------')
         #####################################    
         #aca meter el codigo de ejecucion de la cola de fenomenos.
         #####################################
@@ -32,6 +35,6 @@ def main():
     
     
 main()
-
+    
 
         
