1189df9390f9f1de1be4c7e5048190840e5c07b9
I-Dayz-I <61361549+I-Dayz-I@users.noreply.github.com>
2021-12-17 01:52:58 -0500

    Revert "Añadiendo el método print map"
    This reverts commit 518137a378d937044fc6a03290f032d923753793.

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import map
 
 
 def worldController():
-    while (True):
+    i=10
+    while (i>0):
         
         value=''
         for val in globals.worldIndividuals:
             value=globals.worldIndividuals[val]
             tempMap=globals.worldMap.movementWithBoundries(value,1) 
             value.resolveIteration(tempMap)
-        
-        globals.worldMap.PrintMap()
+        i-=1
         #####################################    
         #aca meter el codigo de ejecucion de la cola de fenomenos.
         #####################################
@@ -22,16 +22,23 @@ def worldController():
 
     
 def main():
+    globals.worldMap=map.Map(5,5,4)
     globals.allSpecies["Alfie"]=especies.Alfie()
     current=globals.allSpecies["Alfie"]
     #current.individuos["Alfie1"].breed()
     #print(len(current.individuos))
-    globals.worldMap=map.Map(5,5,4)
+    
     worldController()
     
     
     
 main()
+    
+KK = map.Map(6,6,2)
 
-
+for i in range(globals.worldMap.SizeX):
+    for j in range(globals.worldMap.SizeY):
+        print(KK.Tiles[i][j].Zone.ZoneType)
+        print(KK.Tiles[i][j].Coordinates)
+        print(KK.Tiles[i][j].ComponentsDict)
         
