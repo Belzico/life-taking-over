52cdde0167f0c3f9ed37b57cb0245506b1ae7edd
I-Dayz-I <61361549+I-Dayz-I@users.noreply.github.com>
2021-12-24 18:41:17 -0500

    Método que halla el promedio de características
    En zona.py se añadió el método que devuelve un diccionario con los valores originales de la especie, un promedio de las características de todos los individuos que se encuentran en esa zona , y un valor que representa la varianza de esos resultados con respecto a los originales

---
+++ graphics.py
@@ -0,0 +1,28 @@
+import tkinter
+import globals
+import zones
+import map
+
+class MapGraphics:
+    def __init__(self, map):
+        return
+        
+    def updateMap(slef):
+        return
+
+
+
+class TileGraphics:
+    def __init__(self):
+        return
+
+
+class IndividuoGrahpics:
+    def __init__(self):
+        return
+    
+class SpeciesGraphics:
+    def __init__(self):
+        return
+    
+    
--- zones.py
+++ zones.py
@@ -1,5 +1,5 @@
 import globals
-
+import misc
 class Zone:
 
     def __init__(self, tileCount, ZoneType):
@@ -8,6 +8,42 @@ class Zone:
         self.Danger = globals.ZoneDanger[ZoneType]
         self.TileList = []
 
+
+
+#El método recibe la especie con la que se trabajará    
+    def startEvolving(self, especie):
+        varianza = -1
+        #Añadiedno la NaturalDefense de la especie original al diccionario
+        AllDictionary = {"NaturalDefense" : especie.naturalDefense}
+        individualsCount = 0
+
+        #Dicionario donde guardaré el promedio de valores de todos los individuos de la especie
+        PromedDictionary = {}
+        
+        #Rellenando los valores promedios de los individuos con todos los que se encuentran en esa zona
+        for i in range(self.TileCount):
+            for j in range(len(self.TileList[i].CreatureList)):
+                if j.especie == especie:
+                    individualsCount +=1
+                    promedDictionary = misc.dictMergeSum(j.naturalDefenseInd, promedDictionary)
+
+        #Dividiendo cada valor entre la cantidad de individuos para hallar el promedio
+        for i in promedDictionary.keys():
+            promedDictionary[i] = promedDictionary[i]/individualsCount
+            
+        #Calculando la varianza de todos los valores
+        for i in promedDictionary.keys():
+           varianza += int(especie.naturalDefense[i])- int(promedDictionary[i])
+        varianza = varianza/individualsCount
+        
+        #añadiendo el valor de varianza y los promedios al diccionario
+        AllDictionary["Varianza"]= varianza
+        AllDictionary["Promedio"]= promedDictionary
+    
+        return AllDictionary
+
+        
+        
     
     def ChangeDanger(self, newDangerInt):
         self.Danger = newDangerInt
