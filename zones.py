25b2255626c08532493520a3c26668c9d7778d8a
I-Dayz-I <61361549+I-Dayz-I@users.noreply.github.com>
2021-12-26 00:36:10 -0500

    Terminando las condiciones de evolución
    Ajustando las condiciones de evolución, y terminando de hallar las varianzas de características

--- zones.py
+++ zones.py
@@ -1,5 +1,6 @@
 import globals
 import misc
+import especies
 class Zone:
 
     def __init__(self, tileCount, ZoneType):
@@ -13,46 +14,85 @@ class Zone:
 #El método recibe la especie con la que se trabajará    
     def startEvolving(self, especie):
         varianzaPromedio = -1
-        #Añadiedno la NaturalDefense de la especie original al diccionario
-        AllDictionary = {"NaturalDefense" : especie.naturalDefense}
+        #Añadiedno la especie original al diccionario
+        AllDictionary = {"EspeciePadre" : especie}
+
+        #Contadores para saber cuantos evoluciono
+        individualsInZone = 0
+        MajorityOfindividuals = 0
+        TileToEvolve = -1
         
-        #Añadiendo la lista de individuos
-        AllDictionary["Individuos"]  = []
-        individualsCount = 0
-        
-        VarianzaDictionary = {}
+        varianzaDictionary = {}
         
 
         #Dicionario donde guardaré el promedio de valores de todos los individuos de la especie
         PromedDictionary = {}
         
         #Rellenando los valores promedios de los individuos con todos los que se encuentran en esa zona
-        for i in range(self.TileCount):
-            for j in range(len(self.TileList[i].CreatureList)):
+        for i in self.TileList:
+            temcount = 0
+            for j in i.CreatureList:
                 if j.especie == especie:
-                    individualsCount +=1
-                    #Añadiendo el individuo a la lista
-                    AllDictionary["Individuos"].append(j)             
+                    temcount += 1
+                    individualsInZone +=1
+                    #Añadiendo el individuo a la lista          
                     promedDictionary = misc.dictMergeSum(j.naturalDefenseInd, promedDictionary)
-                    
-
-        
-        #Rellenando todos los valores
+            
+            if temcount > MajorityOfindividuals:
+                MajorityOfindividuals = temcount
+                TileToEvolve = i
         
         
         #Dividiendo cada valor entre la cantidad de individuos para hallar el promedio
         for i in promedDictionary.keys():
-            promedDictionary[i] = promedDictionary[i]/individualsCount
+            promedDictionary[i] = promedDictionary[i]/individualsInZone
             
-        #Calculando la varianza de todos los valores
+            
+        #Añadiendo el promedio al diccionario
+        AllDictionary["Promedio"]= promedDictionary
+
+        
+        #Rellenando todos los valores de varianza
+        for i in range(self.TileCount):
+            for j in range(len(self.TileList[i].CreatureList)):
+                if j.especie == especie:
+                    #Añadiendo el individuo a la lista        
+                    tempdict = misc.dictMergeSubstract(j.naturalDefenseInd, promedDictionary)
+                    varianzaDictionary = misc.dictMergeSum(tempdict,varianzaDictionary)
+        
+        #Terminando de hallar la varianza
+        for i in varianzaDictionary.keys():
+            varianzaDictionary[i] = varianzaDictionary[i]/individualsInZone
+        
+        AllDictionary["Varianza"] = varianzaDictionary
+        
+        
+        #Hallando la varianza general con respecto a la especie padre
         for i in promedDictionary.keys():
            varianzaPromedio += int(especie.naturalDefense[i])- int(promedDictionary[i])
-        varianza = varianzaPromedio/individualsCount
+        varianza = varianzaPromedio/individualsInZone
         
-        #añadiendo el valor de varianza y los promedios al diccionario
 
-        AllDictionary["Promedio"]= promedDictionary
+
+        #Si la varianza general es lo suficientemente grande entonces evoluciona
+        if varianza >= 5:
+            if MajorityOfindividuals <= (len(especie.individuos)/2):
+                TileToEvolve.deleteCreaturesEspecies(MajorityOfindividuals, especie)
+                especies.Especies(MajorityOfindividuals,TileToEvolve.Coordinates[0],TileToEvolve.Coordinates[1],AllDictionary)
+            
+            else:
+                TileToEvolve.deleteCreaturesEspecies(MajorityOfindividuals/2, especie)
+                especies.Especies(MajorityOfindividuals/2,TileToEvolve.Coordinates[0],TileToEvolve.Coordinates[1],AllDictionary)
+            
     
+        
+    
+        
+    
+  
+        #Calculando el promedio de la varianza de todos los valores
+
+
     
         return AllDictionary
 
