cf6d6b877520dbbd2d0f2bd858a76e54ba88c65f
I-Dayz-I <61361549+I-Dayz-I@users.noreply.github.com>
2021-12-25 20:53:13 -0500

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -1,11 +1,33 @@
-import tkinter
+from tkinter import *
 import globals
 import zones
 import map
 
 class MapGraphics:
     def __init__(self, map):
+      
+        # Create an instance of tkinter frame or window
+        win=Tk()
+
+        # Set the size of the tkinter window
+        win.geometry("700x350")
+
+        # Create a canvas widget
+        canvas=Canvas(win, width=500, height=300)
+        canvas.pack()
+        
+        AmountofLines
+        
+        # Add a line in canvas widget
+        for i in range(map.SizeX):
+            for j in range(map.SizeY):
+                
+
+       
+        
+        
         return
+      
         
     def updateMap(slef):
         return
