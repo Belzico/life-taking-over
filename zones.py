ceb7a3481cb02deca63144396fbb376215b50428
Belzico <61699417+Belzico@users.noreply.github.com>
2022-02-07 01:29:36 -0500

    mini

--- zones.py
+++ zones.py
@@ -170,7 +170,7 @@ class Zone:
         randomIndex = randint(0 , int(ElementsPctTupleList[-1][1]))
         
         for i in ElementsPctTupleList:
-            if i[1]<= randomIndex:
+            if randomIndex<=i[1] :
                 return i[0]
         
         return 'Solar Light'
