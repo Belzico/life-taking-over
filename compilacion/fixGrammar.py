051b2796783d1ab9a0870a6761362f7ab1b0a5f7
I-Dayz-I <61361549+I-Dayz-I@users.noreply.github.com>
2022-02-23 18:15:32 -0500

    Update fixGrammar.py

--- compilacion/fixGrammar.py
+++ compilacion/fixGrammar.py
@@ -19,3 +19,34 @@ productions={
     "stat_list":["stat",TokeTypes.tokSemicolon]
     
 }
+
+
+
+
+class Terminal:
+    def __init__(self, Name, Type):
+        self.name = Name
+        self.type = Type
+
+
+class Production:
+
+    def __init__(self, NonTerminal ,Components):
+        self.components = Components
+        self.nonTerminal = NonTerminal
+
+
+class NonTerminal:
+    def __init__(self, Name, Productions):
+        self.name = Name
+        self.productions = Productions
+
+    def __iadd__(self, prod: Production):
+        self.add(prod)
+        return self
+
+
+class Grammar:
+    def __init__(self, nonTList, Head):
+        self.nonTList = nonTList
+        self.head = Head 
